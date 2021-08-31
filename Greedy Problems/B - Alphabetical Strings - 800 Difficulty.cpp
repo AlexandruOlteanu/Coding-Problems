0fcd9087c489566b45d8df44b4d06db732be44ad
Alexandru Olteanu <86569420+AlexandruOlteanu@users.noreply.github.com>
2021-08-31 11:13:31 +0300

    Add files via upload

---
+++ Greedy Problems/B - Alphabetical Strings - 800 Difficulty.cpp
@@ -0,0 +1,82 @@
+/*
+    Programmer : Alexandru_Olteanu
+    Link To Problem : https://codeforces.com/contest/1547/problem/B
+*/
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+ll const mod=1000000007LL;
+ll const mod2 = 100000000LL;
+ll const md=998244353LL;
+ll mypowr(ll a,ll b) {ll res=1;if(b<0)b=0;a%=mod; assert(b>=0);
+for(;b;b>>=1){if(b&1)res=res*a%mod;a=a*a%mod;}return res;}
+ll mypow(ll a,ll b) {ll res=1;if(b<0)b=0;assert(b>=0);
+for(;b;b>>=1){if(b&1)res=res*a;a=a*a;}return res;}
+ifstream in("input.txt");
+ofstream out("output.txt");
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define fi first
+#define se second
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+
+//#define cin in
+//#define cout out
+
+const ll maxn = 1e3 + 2;
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    cout<<fixed<<setprecision(15);
+
+
+    int test = 1;
+    cin>>test;
+    while(test--){
+         string s;
+         cin>>s;
+         int n = s.size();
+         string c = s;
+         sort(c.begin(), c.end());
+         bool ok = 1;
+         int pos = 0;
+         for(int i = 0; i < n; ++i){
+             if(c[i] != char('a' + i)){
+                 ok = 0;
+             }
+             if(s[i] == 'a')pos = i;
+         }
+         if(ok){
+             int i = pos - 1, j = pos + 1;
+             char p = 'b';
+             --n;
+             int n1 = n + 1;
+             while(n){
+                 bool ok2 = 0;
+                 if(i >= 0){
+                     if(s[i] == p)--i,++p,ok2 = 1;
+                 }
+                 if(!ok2 && j < n1){
+                     if(s[j] == p)++j,++p,ok2 = 1;
+                 }
+                 if(!ok2)ok = 0;
+                 --n;
+             }
+         }
+         if(ok){
+             cout<<"YES\n";
+         }
+         else{
+             cout<<"NO\n";
+         }
+    }
+
+    return 0;
+}
