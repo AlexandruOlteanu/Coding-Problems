2b896976ece7b2b47e0e65b53bb0ac11b61856f2
Alexandru Olteanu <86569420+AlexandruOlteanu@users.noreply.github.com>
2021-08-31 18:24:23 +0300

    Add files via upload

---
+++ Greedy Problems/B. Segment Occurrences - 1300 Difficulty.cpp
@@ -0,0 +1,73 @@
+/*
+    Programmer : Alexandru_Olteanu
+*/
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+ll const mod=1000000007LL;
+ll const mod2 = 100000000LL;
+ll const md=998244353LL;
+ll mypowr(ll a,ll b) {ll res=1;if(b<0)b=0;a%=mod; assert(b>=0);
+for(;b;b>>=1){if(b&1)res=res*a%mod;a=a*a%mod;}return res;}
+ll mypow(ll a,ll b) {ll res=1;if(b<0)b=0;assert(b>=0);
+for(;b;b>>=1){if(b&1)res=res*a;a=a*a;}return res;}
+ifstream in("input.txt");
+ofstream out("output.txt");
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define fi first
+#define se second
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+
+//#define cin in
+//#define cout out
+
+const ll infll = 9e18;
+const int inf = 2e9;
+const ll maxn = 1e3 + 2;
+
+int p[maxn];
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    cout<<fixed<<setprecision(15);
+    
+    
+    int test = 1;
+    //cin>>test;
+    for(int tt = 1; tt <= test; ++tt){
+        int n, m, k;
+        cin>>n>>m>>k;
+        string s, t;
+        cin>>s>>t;
+        int a = 0;
+        while(s.find(t, a) < n){
+            int tpos = s.find(t, a);
+            ++p[tpos];
+            a = tpos + 1;
+            if(a >= n)break;
+        }
+        for(int i = 0; i < n; ++i){
+            p[i] += p[i - 1];
+        }
+        for(int i = 1; i <= k; ++i){
+            int x, y;cin>>x>>y;--x, --y;
+            if(y - x + 1 < m){
+                cout<<"0\n";
+                continue;
+            }
+            cout<<p[y - m + 1] - p[x - 1]<<'\n';
+        }
+
+
+    }
+    return 0;
+}
