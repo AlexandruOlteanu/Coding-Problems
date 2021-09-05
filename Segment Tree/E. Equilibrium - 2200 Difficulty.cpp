facf2edc3ee379ec8b1808202cfde3140b016fb8
Alexandru Olteanu <86569420+AlexandruOlteanu@users.noreply.github.com>
2021-09-05 12:40:25 +0300

    Update E. Equilibrium - 2200 Difficulty.cpp

--- Segment Tree/E. Equilibrium - 2200 Difficulty.cpp
+++ Segment Tree/E. Equilibrium - 2200 Difficulty.cpp
@@ -1,6 +1,5 @@
 /*
     Programmer : Alexandru_Olteanu
-    Link To Problem : https://codeforces.com/contest/1556/problem/E
 */
 #include<bits/stdc++.h>
 using namespace std;
@@ -16,14 +15,11 @@ ll mypow(ll a,ll b) {ll res=1;if(b<0)b=0;assert(b>=0);
 for(;b;b>>=1){if(b&1)res=res*a;a=a*a;}return res;}
 ifstream in("input.txt");
 ofstream out("output.txt");
-#define mp make_pair
 #define pb push_back
-#define pf push_front
 #define fi first
 #define se second
 #define all(x) x.begin(),x.end()
 #define rall(x) x.rbegin(), x.rend()
-
 
 //#define cin in
 //#define cout out
@@ -32,39 +28,83 @@ const ll infll = 9e18;
 const int inf = 2e9;
 const ll maxn = 1e5 + 2;
 
-ll a[maxn];
-ll node[3 * maxn + 5];
-ll node1[3 * maxn + 5];
+/*
+    Template created by Alexandru Olteanu
+*/
+template<typename A>
+struct SegmentTree{
+    vector<A> array;
+    vector<A> tree;
+    vector<A> lazy;
 
+    SegmentTree(int n){
+        array.resize(n, 0);
+        tree.resize(4 * n + 1, 0);
+        lazy.resize(4 * n + 1, 0);
+    }
 
-void segmentmin(int x){
-    if(x == 0)return;
-    if(node[(x - 1) / 2] = min(node[(x - 1) / 2], node[x]));
-    segmentmin((x - 1) / 2);
-    return;
-}
+    A function(A a, A b, int p){
+        if(p == 1) return (a > b ? a : b);
+        return (a < b ? a : b);
+    }
 
-void segmentmax(int x){
-    if(x == 0)return;
-    if(node1[(x - 1) / 2] = max(node1[(x - 1) / 2], node1[x]));
-    segmentmax((x - 1) / 2);
-    return;
-}
-ll ans = infll, ans1 = -infll;
-
-void find(int nd, int x, int y, int st, int dr){
-    if(x > y || y < st || x > dr)return;
-    if(x >= st && y <= dr){
-        ans = min(ans, node[nd]);
-        ans1 = max(ans1, node1[nd]);
+    void build_segment(int node, int l, int r, int p){
+        if(l == r){
+            tree[node] = array[l];
+            return;
+        }
+        int mid = l + (r - l) / 2;
+        build_segment(node * 2, l, mid, p);
+        build_segment(node * 2 + 1, mid + 1, r, p);
+        tree[node] = function(tree[node * 2], tree[node * 2 + 1], p);
         return;
     }
-    find(nd * 2 + 1, x, (y - x) / 2 + x, st, dr);
-    find(nd * 2 + 2, x + (y - x) / 2 + 1, y, st, dr);
-    return;
-}
 
+    void push_segment(int node, int l, int r, int p){
+        return;
+        if(lazy[node] != 0){
+            tree[node] = (r - l + 1) - tree[node];
+            if(l != r){
+                lazy[node * 2] ^= 1;
+                lazy[node * 2 + 1] ^= 1;
+            }
+            lazy[node] = 0;
+        }
+        return;
+    }
 
+    void update_segment(int node, int l, int r, int L, int R, int p){
+        push_segment(node, l, r, p);
+        if(r < L || l > R)return;
+        if(l >= L && r <= R){
+            lazy[node] ^= 1;
+            push_segment(node, l, r, p);
+            return;
+        }
+        int mid = l + (r - l) / 2;
+        update_segment(node * 2, l, mid, L, R, p);
+        update_segment(node * 2 + 1, mid + 1, r, L, R, p);
+        tree[node] = function(tree[node * 2], tree[node * 2 + 1], p);
+        return;
+    }
+
+    A ask_segment(int node, int l, int r, int L, int R, int p){
+        push_segment(node, l, r, p);
+        if(r < L || l > R){
+            if(p == 1)return -infll;
+            else return infll;
+        }
+        if(l >= L && r <= R){
+            return tree[node];
+        }
+        int mid = l + (r - l) / 2;
+        A ask1 = ask_segment(node * 2, l, mid, L, R, p);
+        A ask2 = ask_segment(node * 2 + 1, mid + 1, r, L, R, p);
+        return function(ask1, ask2, p);
+    }
+};
+
+int a[maxn];
 
 int main()
 {
@@ -72,54 +112,43 @@ int main()
     cin.tie(0);cout.tie(0);
     cout<<fixed<<setprecision(15);
     
-
+    
     int test = 1;
     //cin>>test;
     for(int tt = 1; tt <= test; ++tt){
-        int n, m;
-        cin>>n>>m;
+        int n, k;
+        cin>>n>>k;
+        SegmentTree<ll> maxi(n + 1), mini(n + 1);
         for(int i = 1; i <= n; ++i){
-            cin>>a[i];
-        }
-        for(int i = 1; i <= n; ++i){
-            int x; cin>>x;
-            a[i] = x - a[i];
-            a[i] += a[i - 1];
-        }
-        ll power = 1;
-        while(power < n){
-            power <<= 1LL;
-        }
-        --power;
-        for(int i = 0; i <= power + n - 1; ++i){
-            node[i] = infll;
-            node1[i] = -infll;
+            cin>>maxi.array[i];
         }
         for(int i = 1; i <= n; ++i){
-            node[power + i - 1] = a[i];
-            node1[power + i - 1] = a[i];
-            segmentmin(power + i - 1);
-            segmentmax(power + i - 1);
+            int x;cin>>x;
+            maxi.array[i] = x - maxi.array[i];
+            maxi.array[i] += maxi.array[i - 1];
+            mini.array[i] = maxi.array[i];
         }
-        for(int i = 1; i <= m; ++i){
+        maxi.build_segment(1, 1, n, 1);
+        mini.build_segment(1, 1, n, 0);
+        for(int i = 1; i <= k; ++i){
             int x, y;cin>>x>>y;
-            if(a[y] != a[x - 1]){
+            if(maxi.array[y] - maxi.array[x - 1] != 0){
                 cout<<"-1\n";
                 continue;
             }
-            find(0, 1, power + 1, x, y);
-            ans -= a[x - 1];
-            ans1 -= a[x - 1];
-            if(ans < 0){
+            ll cnt = maxi.ask_segment(1, 1, n, x, y, 1);
+            ll cnt1 = mini.ask_segment(1, 1, n, x, y, 0);
+            cnt -= maxi.array[x - 1];
+            cnt1 -= maxi.array[x - 1];
+            if(cnt1 < 0){
                 cout<<"-1\n";
-                ans = infll;
-                ans1 = -infll;
                 continue;
             }
-            cout<<ans1<<'\n';
-            ans = infll;
-            ans1 = -infll;
+            cout<<cnt<<'\n';
         }
+
+
+
     }
     return 0;
 }
