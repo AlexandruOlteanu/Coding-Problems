4384034365427ece4a5900939db2fec232ad291b
Alexandru Olteanu <86569420+AlexandruOlteanu@users.noreply.github.com>
2021-09-09 20:19:07 +0300

    Create A. Domino Disaster - 800 Difficulty.cpp

---
+++ Greedy Problems/A. Domino Disaster - 800 Difficulty.cpp
@@ -0,0 +1,66 @@
+/*
+    Programmer : Alexandru_Olteanu
+    Link To Problem : https://codeforces.com/contest/1567/problem/A
+*/
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+ll const mod=1000000007LL;
+ll const mod2 = 100000000LL;
+ll const md=998244353LL;
+ll mypowr(ll a,ll b) {ll res=1;if(b<0)b=0;a%=mod; assert(b>=0);
+for(;b;b>>=1){if(b&1)res=res*a%mod;a=a*a%mod;}return res;}
+ll mypow(ll a,ll b) {ll res=1;if(b<0)b=0;assert(b>=0);
+for(;b;b>>=1){if(b&1)res=res*a;a=a*a;}return res;}
+ifstream in("input.txt");
+ofstream out("output.txt");
+#define pb push_back
+#define fi first
+#define se second
+#define all(x) x.begin(),x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+//#define cin in
+//#define cout out
+
+const ll infll = 9e18;
+const int inf = 2e9;
+const ll maxn = 1e6 + 2;
+
+char c[105];
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    cout<<fixed<<setprecision(15);
+    
+    
+    int test = 1;
+    cin>>test;
+    for(int tt = 1; tt <= test; ++tt){
+        int n;
+        cin>>n;
+        string s;cin>>s;
+        s = "#" + s;    
+        for(int i = 1; i <= n; ++i){
+            c[i] = '#';
+            if(s[i] == 'U')c[i] = 'D';
+            if(s[i] == 'D')c[i] = 'U';
+        }
+        for(int i = 2; i <= n; ++i){
+            if(c[i] == '#' && c[i - 1] == '#'){
+                c[i] = 'R', c[i - 1] = 'L';
+            }
+        }
+        for(int i = 1; i <= n; ++i){
+            cout<<c[i];
+        }
+        cout<<'\n';
+
+
+    }
+    return 0;
+}
