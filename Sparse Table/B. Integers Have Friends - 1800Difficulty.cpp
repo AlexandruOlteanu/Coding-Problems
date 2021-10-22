0066724eb93ec34777d5a7ffa4690e156059de8e
Alexandru Olteanu <alexandruolteanu2001@gmail.com>
2021-10-22 13:10:28 +0300

    Update B. Integers Have Friends - 1800Difficulty.cpp

--- Sparse Table/B. Integers Have Friends - 1800Difficulty.cpp
+++ Sparse Table/B. Integers Have Friends - 1800Difficulty.cpp
@@ -1,89 +1,142 @@
 /*
     Programmer : Alexandru Olteanu
-    Link To Problem : https://codeforces.com/contest/1548/problem/B
 */
 #include<bits/stdc++.h>
 using namespace std;
+// GCC Optimizations
+// #pragma GCC optimize("Ofast");
+// #pragma GCC target("fma,sse,sse2,sse3,ssse3,sse4,popcnt")
+// #pragma GCC target("abm,mmx,avx,avx2,tune=native")
+// #pragma GCC optimize(3)
+// #pragma GCC optimize("inline")
+// #pragma GCC optimize("-fgcse")
+// #pragma GCC optimize("-fgcse-lm")
+// #pragma GCC optimize("-fipa-sra")
+// #pragma GCC optimize("-ftree-pre")
+// #pragma GCC optimize("-ftree-vrp")
+// #pragma GCC optimize("-fpeephole2")
+// #pragma GCC optimize("-ffast-math")
+// #pragma GCC optimize("-fsched-spec")
+// #pragma GCC optimize("unroll-loops")
+// Useful
+mt19937 rng((unsigned int) chrono::steady_clock::now().time_since_epoch().count());
+#define FastEverything  ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define HighPrecision cout<<fixed<<setprecision(17);
 typedef long long ll;
 typedef pair<int,int> pii;
-typedef pair<ll,ll> pll;
 ll const mod=1000000007LL;
 ll const mod2 = 100000000LL;
 ll const md=998244353LL;
-ll mypowr(ll a,ll b) {ll res=1;if(b<0)b=0;a%=mod; assert(b>=0);
-for(;b;b>>=1){if(b&1)res=res*a%mod;a=a*a%mod;}return res;}
+ll mypowr(ll a,ll b, ll mod1) {ll res=1;if(b<0)b=0;a%=mod1; assert(b>=0);
+for(;b;b>>=1){if(b&1)res=res*a%mod1;a=a*a%mod1;}return res;}
 ll mypow(ll a,ll b) {ll res=1;if(b<0)b=0;assert(b>=0);
 for(;b;b>>=1){if(b&1)res=res*a;a=a*a;}return res;}
-ifstream in("input.txt");
-ofstream out("output.txt");
-#define mp make_pair
 #define pb push_back
-#define pf push_front
 #define fi first
 #define se second
 #define all(x) x.begin(),x.end()
 #define rall(x) x.rbegin(), x.rend()
 
-
-//#define cin in
-//#define cout out
+// ifstream in("input.txt");
+// ofstream out("output.txt");
+// #define cin in
+// #define cout out
 
 const ll infll = 9e18;
 const int inf = 2e9;
-const ll maxn = 2e5 + 2;
+const ll maxn = 1e5 + 2;
 
-ll a[maxn];
-ll st[maxn][20];
+/* 
+    Template created by Alexandru Olteanu
+*/
+template<typename A>
+struct SparseTable{
+    
+    vector<vector<A>> matrix;
+    vector<A> array;
+    vector<A> log_values;
+    
+    SparseTable(int n){
+        int max_log = 0, power = 1;
+        while(power <= n){
+            power <<= 1;
+            ++max_log;
+        }
+        array.resize(n + 1);
+        log_values.resize(n + 1);
+        matrix.resize(n + 1);
+        for(int i = 1; i <= n; ++i){
+            matrix[i].resize(max_log);
+        }
+    }
+    A func(A a, A b){
+        return __gcd(a, b);//Probably it needs changes
+    }
+    
+    void build(int l, int r){
+        int max_log = 0, power = 1;
+        int n = r - l + 1;
+        while(power <= n){
+            power <<= 1;
+            ++max_log;
+        }
+        int number = 0;
+        power = 1;
+        for(int i = 1; i <= n; ++i){
+            while(power <= i){
+                power <<= 1;
+                ++number;
+            }
+            power >>= 1;
+            --number;
+            log_values[i] = number;
+        }
+        for(int j = 0; j < max_log; ++j){
+            for(int i = l; i <= r - (1 << j) + 1; ++i){
+                if(j == 0){
+                    matrix[i][j] = array[i];
+                    continue;
+                }
+                matrix[i][j] = func(matrix[i][j - 1], matrix[i + (1 << (j - 1))][j - 1]);
+            }
+        }
+    }
+
+    ll get(A start, A finish){
+        ll distance = finish - start + 1;
+        ll biggest_power = log_values[distance];
+        return func(matrix[start][biggest_power], matrix[finish - (1 << biggest_power) + 1][biggest_power]);
+    }
+};
+
+
+
+
 
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);cout.tie(0);
-    cout<<fixed<<setprecision(15);
-    
-    auto f = [&] (ll x, ll y){
-        return __gcd(x, y);
-    };
-    auto good = [&] (int x, int y){
-        ll answer = 0;
-        while(x <= y){
-            ll u = (y - x + 1);
-            ll t = 1, sum = 0;
-            while(t <= u){
-                t *= 2;
-                ++sum;
-            }
-            --sum;
-            answer = f(answer, st[x][sum]);
-            x += (1<<sum);
-        }
-        if(answer > 1)return true;
-        return false;
-    };
+    FastEverything
+    HighPrecision
+
 
     int test = 1;
     cin>>test;
-    while(test--){
+    for(int tt = 1; tt <= test; ++tt){
         int n;
         cin>>n;
+        SparseTable<ll> st(n);
         for(int i = 1; i <= n; ++i){
-            cin>>a[i];
+            cin>>st.array[i];
+        }
+        for(int i = 2; i <= n; ++i){
+            st.array[i - 1] = abs(st.array[i] - st.array[i - 1]); 
         }
         --n;
-        int logmax = 32 - __builtin_clz(n);
-        for(int j = 0; j <= logmax; ++j){
-            for(int i = 1; i + (1<<j) - 1 <= n; ++i){
-                if(j == 0){
-                    st[i][0] = f(0, abs(a[i + 1] - a[i]));
-                    continue;
-                }
-                st[i][j] = f(st[i][j - 1], st[i + (1<<(j - 1))][j - 1]);
-            }
-        }
+        st.build(1, n);
         int cnt = 0;
         int i = 1, j = 1;
         while(i <= n && j <= n){
-            if(good(i, j)){
+            if(st.get(i, j) > 1){
                 cnt = max(cnt, j - i + 1);
                 ++j;
             }
@@ -94,7 +147,7 @@ int main()
         }
         cout<<cnt + 1<<'\n';
 
-    }
+    }       
 
     return 0;
-}
+} 
